85a8fba7ea5b152832c2eee2f77dcb656c6b6ff6
TimothyXia9 <tianqi5902@gmail.com>
2025-07-23 20:42:10 -0400

    feat: 添加后端部署

--- calorie_tracker/settings.py
+++ calorie_tracker/settings.py
@@ -132,6 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Use WhiteNoise for static file serving in production
+if not DEBUG:
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
