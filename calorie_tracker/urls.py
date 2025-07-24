65c1245b949750e19896baa005e57c0fcd20a3ae
TimothyXia9 <tianqi5902@gmail.com>
2025-07-23 21:14:35 -0400

    feat: 添加健康检查

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -19,8 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
+    path("api/v1/", health_check),
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("accounts.urls")),
     path("api/v1/users/", include("accounts.urls")),
