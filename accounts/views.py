b52c0aab54762ebfd3305239d0f64e2ffa5ba98b
TimothyXia9 <tianqi5902@gmail.com>
2025-07-30 12:42:20 -0400

    feat: 移除未使用的装饰器

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status, generics, permissions
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
